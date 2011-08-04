5d0267039a8432ad8912b51ab51c5cf8d6238b9e
filip Lagerlov <filip.lagerlov@gmail.com>
2011-08-05 00:04:08 +0200

    some buggs time for some style

--- server.js
+++ server.js
@@ -2,18 +2,8 @@ var net = require('net');
 var express = require('express');
 var io = require('socket.io');
 
-;
-  
-  exports.NewMudcon  = function ()
-  {
- 	 var currentSocketClient = net.createConnection(4242,"mume.org"); 
-  	 mudclient.push(currentSocketClient);
-	 
-	 currentSocketClient.addListener("data",function(d){ socket.write(d)});
-    currentSocketClient.write(d,"utf8",function(d) {})
- 
-}
 
+  
 
 var app = require('express').createServer();
 
