e39f28207cf727a9460b467de196bbfc8dc743de
filip Lagerlov <filip.lagerlov@gmail.com>
2011-08-10 20:58:57 +0200

    keymap added

--- server.js
+++ server.js
@@ -8,47 +8,51 @@ var fs = require('fs');
 var app = require('express').createServer();
 
 exports.StartSite = function() {
-
    app.listen(9000);
-      
 }
 
 app.get('/',function(req,res) { 
-
-	res.partial('layout.jade',{});
+   res.partial('layout.jade',{});
 })
 
 app.get('/client.js',function(req,res) {
 	
-	fs.readFile("js/client.js",function(err,d) {
-	   res.end(d +"");	
-	});
+  fs.readFile("js/client.js",function(err,d) {
+     res.end(d +"");	
+  });
 });
 app.get('/style.css',function(req,res) {
 	
-	fs.readFile("css/style.css",function(err,d) {
-		res.end(d+ "");		
-	});
+   fs.readFile("css/style.css",function(err,d) {
+      res.end(d+ "");		
+   });
+});
+app.get('/keymap.json',function(req,res) {
+
+   fs.readFile("js/keymap.json",function(err,d) {
+      res.end(d+"");
+   })
 });
 
 exports.StartSocket = function()
 {
-	var clients =[];
-	var socket = io.listen(app);
+   var clients =[];
+   var socket = io.listen(app);
 	
-	socket.sockets.on('connection',function(client) {
+   socket.sockets.on('connection',function(client) {
 		        
-	client.mume  = net.createConnection(4242,"mume.org"); 
+   client.mume  = net.createConnection(4242,"mume.org"); 
    client.mume.addListener("data",function(d){       
         
-        var newstring =  d.toString().split("\r\n").join("<br>")
+  var newstring =  d.toString().split("\r\n").join("<br>")
         client.emit("data",{data:newstring});  
    });
                                
-	clients.push(client);
-	client.on('newcommand',function(d) {client.mume.write(d.cmd+"","utf8",function(d) {} )});
-	client.on('disconnect',function() {} );		
-	});	
+   clients.push(client);
+   client.on('newcommand',function(d) {
+   client.mume.write(d.cmd+"","utf8",function(d) {} )}); 
+   client.on('disconnect',function() {});		
+ });	
 }
 
 var server =require('./server.js'); 
