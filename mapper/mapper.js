90ddc29c6e1d2af748b6abdd151c7a85169fb891
filip Lagerlov <filip.lagerlov@gmail.com>
2011-08-14 17:07:20 +0200

    Mapper module

--- mapper/mapper.js
+++ mapper/mapper.js
@@ -1,26 +1,43 @@
-    var select = require('soupselect').select,
+/* Writer: Filip Lagerlov
+ * This is a mapper for the client based on node soup select. 
+ * calling the getMapper() function returns a Mapper object.
+ * 
+ *  Todo:    
+ *  Map the xml input
+ *  save input to file;
+ *
+ */  
+
+
+   var select = require('soupselect').select,
         htmlparser = require("htmlparser"),
         http = require('http'),
         sys = require('sys');
-
- var body = "<name>sven</name>"
- exports.getMapper = function()
-{
   
-  var handler = new htmlparser.DefaultHandler(function(err,dom) {
-   
-    if(err) {
-     sys.debug("Error: " + err);
-    }else {
-      var title = select(dom,'name');
-      console.log(title[0]);
-    }
 
-   
-   });
-   var parser = new htmlparser.Parser(handler);
-   parser.parseComplete(body);   
-}       
+exports.getMapper = function() {
+      return new Mapper();
+}
 
-var a = require('./mapper.js');
-a.getMapper(); 
+
+var Mapper = function () {
+
+   this.handler = new htmlparser.DefaultHandler(function(err,dom) {
+                     if(err) {}
+		      else { 
+                        var roomName = select(dom,'name');
+			 console.log(title[0]);
+                      }
+                  });
+
+   this.parser = new htmlparser.Parser(this.handler);
+   this.fetchData = function (data) {
+			this.parser.parseComplete(data);
+    	            }	
+
+}
+/*       
+Example:
+var a = require('./mapper.js').getMapper();
+a.fetchData("<name>hej</name>");
+*/ 
